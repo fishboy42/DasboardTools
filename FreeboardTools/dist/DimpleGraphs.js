57c9b99e2cb68f207d046455fa877db95f0523a8
Sean McDermid <fish_boy42@hotmail.com>
2017-05-22 20:25:45 -0600

    sync

--- FreeboardTools/dist/DimpleGraphs.js
+++ FreeboardTools/dist/DimpleGraphs.js
@@ -545,7 +545,9 @@
 	function getAbsolutePath() {
 	    var loc = window.location;
 	    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-	    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+	    var x = loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+	    alert(x);
+	    return x;
 	}
 
 }());
